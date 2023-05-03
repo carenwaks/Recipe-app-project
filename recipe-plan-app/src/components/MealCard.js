005cc25bc7badec47f8abe21d9ded9eb911ce6d4
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-05-03 12:39:41 +0300

    components: new components added

---
+++ recipe-plan-app/src/components/MealCard.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function MealCard () {
+    return <h3>Meal card item</h3>
+}
+export default MealCard
---
+++ recipe-plan-app/src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MealCard from "./MealCard";
+
+function Search () {
+    return <div>
+        <h3>Search</h3>
+        
+        <MealCard/>
+    </div>
+}
+
+export default Search
